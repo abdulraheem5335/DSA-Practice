04c82e49be260a8cf3f9d79442de5540c79aa764
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-06-28 15:57:54 +0500

    Added _8point2.cpp

---
+++ 8 Struct/_8point2.cpp
@@ -0,0 +1,44 @@
+// **********  ARRAY OF STRUCTURE ************
+
+
+#include<iostream>
+using namespace std;
+
+// *********** 8.9 Array of Book struct *********
+/*struct Book{
+    int id;
+    int pages;
+    float price;
+};*/
+
+int main(){
+
+    // *********** 8.9 Array of Book struct *********
+    /*Book books[5];
+
+    cout << "ENTER THE RECORD OF 5 BOOKS!" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Book " << i + 1 << endl;
+        cout << "Enter ID: ";
+        cin >> books[i].id;
+        cout << "Enter Pages: ";
+        cin >> books[i].pages;
+        cout << "Enter Price: ";
+        cin >> books[i].price;
+        cout << endl;
+    }
+
+    cout << endl << endl;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Book " << i + 1 << endl;
+        cout << "ID: " <<books[i].id << endl;;
+        cout << "Pages: " << books[i].pages << endl;
+        cout << "Price: " << books[i].price<< endl;
+        cout << endl;
+    }*/
+
+    return 0;
+}
