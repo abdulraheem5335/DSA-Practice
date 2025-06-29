772d8e10ddc6ed8849ad5569738912697bf03d2e
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-06-29 14:58:04 +0500

    Added Functions

---
+++ 9 Functions/_9point3.cpp
@@ -0,0 +1,19 @@
+// ********* FUNCTIONS **********
+
+#include<iostream>
+using namespace std;
+
+// ********** 9.1 Display Messsage using Func *********
+// void message();
+
+int main(){
+
+    // ********** 9.1 Display Messsage using Func *********
+    // message();
+    return 0;
+}
+
+// ********** 9.1 Display Messsage using Func *********
+/*void message(){
+    cout << "Programming is interesting! :)" << endl;
+}*/
