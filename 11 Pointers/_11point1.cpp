1edae7940f611868acdec6721398820d6e1263ea
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-06-30 16:40:08 +0500

    Added Pointers

---
+++ 11 Pointers/_11point1.cpp
@@ -0,0 +1,13 @@
+// ************ POINTERS ************
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n = 10;
+    cout << "The value of n: " << n << endl;
+    cout << "The address of n: " << &n << endl;
+    return 0;
+}
